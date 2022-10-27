1427bcd241fe40e35ca63c3258d0069f12ce057b
CarolSN <79257314+aanacarolina@users.noreply.github.com>
2022-10-27 20:44:32 -0300

    including comments
    vai dar certo!

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anacaro3 <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/21 23:25:12 by anacaro3          #+#    #+#             */
+/*   Updated: 2022/10/21 23:25:15 by anacaro3         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char	*read_line(char *str)
+char	*read_line(char *str) //a parte que trata a keep -- pq so quero devolver a linha até \n
 	{
 	int		i;
 	char	*s;
@@ -10,7 +22,7 @@ char	*read_line(char *str)
 		return (NULL);
 	while (str[i] != '\n' && str[i])
 		i++;
-	s = (char *)malloc(sizeof(char) * (i + 2));
+	s = (char *)malloc(sizeof(char) * (i + 2)); // sao pq a contagem começa em zero (index) dai 
 	if (!s)
 		return (NULL);
 	i = 0;
@@ -43,7 +55,7 @@ char	*extra_letters(char *str)
 		free(str);
 		return (NULL);
 	}
-	s = (char *)malloc(sizeof(char) * (ft_strlen(str) - i + 1));
+	s = (char *)malloc(sizeof(char) * (ft_strlen(str) - i + 1)); //
 	if (!s)
 		return (NULL);
 	i++;
@@ -54,7 +66,8 @@ char	*extra_letters(char *str)
 	return (s);
 }
 
-char	*join_strings(int fd, char *str)
+//junta o que ja leu de buffer zise  e continua lendo até encontrar o\n  e libera do ultimo se for \n
+char	*join_strings(int fd, char *keep)
 {
 	char	*buffer;
 	int		read_bytes;
@@ -62,8 +75,8 @@ char	*join_strings(int fd, char *str)
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
 		return (NULL);
-	read_bytes = 1;
-	while (read_bytes > 0 && !ft_strchr(str, '\n'))
+	read_bytes = 1; //pq senao nao entra no while
+	while (read_bytes > 0 && !ft_strchr(keep, '\n'))
 	{
 		read_bytes = read(fd, buffer, BUFFER_SIZE);
 		if (read_bytes < 0)
@@ -72,10 +85,10 @@ char	*join_strings(int fd, char *str)
 			return (NULL);
 		}
 		buffer[read_bytes] = '\0';
-		str = ft_strjoin(str, buffer);
+		keep = ft_strjoin(keep, buffer); //buffer recebe o q foi lido ( = line) = / keep (stash) = eh a linha inteira
 	}
 	free(buffer);
-	return (str);
+	return (keep);
 }
 
 char	*get_next_line(int fd)
