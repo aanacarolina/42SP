c45f7d7bfb6b70232e53d4fb3aa7de050cce3590
CarolSN <79257314+aanacarolina@users.noreply.github.com>
2023-02-23 21:56:42 -0300

    adding struct to fdf.h

--- fdf/fdf.h
+++ fdf/fdf.h
@@ -1,6 +1,23 @@
 #include <mlx.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <X11/keysym.h>
+#include <X11/X.h>
 
-# define ESC_KEY 65307
-# define X_KEY 120
+# define ESC_KEY ESC
+
+
+typedef struct s_data
+{
+	void *mlx_ptr;
+	void *win_ptr;
+	int win_width;
+	int win_height;
+} t_data;
+
+typedef struct s_position
+{
+	int x;
+	int y;
+	int color;
+} t_position;
