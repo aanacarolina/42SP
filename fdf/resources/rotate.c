3885f90b9bf85405fd52a67fdb09e28d8604f393
aanacarolina <aanacarolina@gmail.com>
2023-03-06 19:46:04 -0300

    [fdf] - removing unnecessary files

--- fdf/resources/rotate.c
+++
@@ -1,7 +0,0 @@
-vec2 rotate(vec2 point, float angle){
-        vec2 rotated_point;
-        rotated_point.x = point.x * cos(angle) - point.y * sin(angle);
-        rotated_point.y = point.x * sin(angle) + point.y * cos(angle);
-        return rotated_point;
-}
-//https://habr.com/ru/post/131931/
