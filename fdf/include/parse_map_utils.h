f5ef02c4c3842530fff27c6643d7cf65eac9f953
aanacarolina <aanacarolina@gmail.com>
2023-03-29 22:01:04 -0300

    normin

--- fdf/include/parse_map_utils.h
+++ fdf/include/parse_map_utils.h
@@ -6,16 +6,17 @@
 /*   By: anacaro3 <anacaro3@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 21:42:11 by anacaro3          #+#    #+#             */
-/*   Updated: 2023/03/29 21:43:41 by anacaro3         ###   ########.fr       */
+/*   Updated: 2023/03/29 22:00:21 by anacaro3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FDF_H
-# define FDF_H
-
+#ifndef PARSE_MAP_UTILS_H
+# define PARSE_MAP_UTILS_H
 
 typedef struct s_parse_map //
 {
 	double	qty_lines;
 	double	qty_cols;
-} t_parse_map;
+}	t_parse_map;
+
+#endif
--- fdf/src/parse_map_utils.c
+++ fdf/src/parse_map_utils.c
@@ -6,11 +6,50 @@
 /*   By: anacaro3 <anacaro3@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 21:35:38 by anacaro3          #+#    #+#             */
-/*   Updated: 2023/03/29 21:41:04 by anacaro3         ###   ########.fr       */
+/*   Updated: 2023/03/29 22:00:44 by anacaro3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/parse_map_utils.h"
 #include "../include/fdf.h"
 
-ft (t_map *mspd , int fd)
+int	count_col(char **split)
+{
+	int	count;
+
+	count = 0;
+	while (split[count] != '\0')
+		count++;
+	return (count);
+}
+
+void	free_split(char **split)
+{
+	int	count;
+
+	count = 0;
+	while (split[count] != '\0')
+		free(split[count]);
+	free(split);
+}
+
+int	ft_counter(t_parse_map *parse_map, int fd)
+{
+	int		qty_row;
+	char	*line;
+	char	**cols;
+	int		qnt_col;
+
+	parse_map->qty_lines = 1;
+	cols = ft_split(line, ' ');
+	parse_map->qty_cols = count_col(cols);
+	free(line);
+	free_split(cols);
+	line = gnl(fd);
+	while (line != NULL)
+	{
+		qty_row++;
+		free(line);
+		line = gnl(fd);
+	}
+}
