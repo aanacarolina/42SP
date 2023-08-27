23b07dbbbca38875d1f7c3b4532b47068671b05e
aanacarolina <aanacarolina@gmail.com>
2023-08-27 03:47:53 -0300

    [norminette] op_push.c: OK!

--- push_swap/src/op_push.c
+++ push_swap/src/op_push.c
@@ -6,7 +6,7 @@
 /*   By: anacaro3 <anacaro3@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 15:29:15 by anacaro3          #+#    #+#             */
-/*   Updated: 2023/08/27 02:14:09 by anacaro3         ###   ########.fr       */
+/*   Updated: 2023/08/27 03:46:19 by anacaro3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,13 @@ void	push_b(t_node **head_a, t_node **head_b)
 		if (*head_b == NULL)
 		{
 			(*llist_b) = (*llist_a);
-			// they are pointers That's why it just points and not become the node
 			(*llist_a) = (*llist_a)->next;
 			(*llist_b)->next = NULL;
 		}
 		else
 		{
 			(*llist_b) = (*llist_a);
-			// b agora aponta pro primeiro node de a
 			(*llist_a) = (*llist_a)->next;
-			// lista A agora aponta pro segundo node de a tipo a deleção desse node de A
 			(*llist_b)->next = temp_b;
 		}
 	}
@@ -55,29 +52,26 @@ void	push_a(t_node **head_a, t_node **head_b)
 		if (*head_a == NULL)
 		{
 			(*llist_a) = (*llist_b);
-			// they are pointers That's why it just points and not become the node
 			(*llist_b) = (*llist_b)->next;
 			(*llist_a)->next = NULL;
 		}
 		else
 		{
 			(*llist_a) = (*llist_b);
-			// b agora aponta pro primeiro node de a
 			(*llist_b) = (*llist_b)->next;
-			// lista A agora aponta pro segundo node de a tipo a deleção desse node de A
 			(*llist_a)->next = temp_a;
 		}
 	}
 }
 
-//**pa (push a): Take the first element at the top of b and put it at the top of a.**
+//pa Take the first element at the top of b and put it at the top of a
 void	pa(t_node **head_a, t_node **head_b)
 {
 	push_a(head_a, head_b);
 	ft_putstr_fd("pa\n", 1);
 }
 
-//**pb (push b): Take the first element at the top of a and put it at the top of b.**
+//pb: Take the first element at the top of a and put it at the top of b
 void	pb(t_node **head_a, t_node **head_b)
 {
 	push_b(head_a, head_b);
