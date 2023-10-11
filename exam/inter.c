66822f6817bbd02865f94166c00172fc14342c3b
carolina.silva <aanacarolina@gmail.com>
2023-10-11 10:04:07 -0300

    inter funcionando thnx fernanda

--- exam/inter.c
+++ exam/inter.c
@@ -1,65 +1,60 @@
 #include <unistd.h>
 
-int is_dup(char *str, char c, int position)
+int	is_dup(char *str, char c, int position)
 {
-    int i = 0;
-    while (i != position)
-    {
-        if (str[i] != c)
-        {
-            i++;
-        }
-        else
-            return (-1);
-    }
-    return (position);
+	int	i;
+
+	i = 0;
+	while (i != position)
+	{
+		if (str[i] != c)
+		{
+			i++;
+		}
+		else
+			return (-1);
+	}
+	return (position);
 }
 
-void ft_inter(char *str1, char *str2)
+void	ft_inter(char *str1, char *str2)
 {
-    int i = 0;
-    int j = 0;
+	int	i;
+	int	j;
 
-    while (str1[i] != '\0')
-    {
-        if (str2[j] != '\0')
-        {
-            i++;
-            j = 0;
-        }
-        else if (str1[i] != str2[j])
-        {
-            j++;
-        }
-        else
-        {
-            if (is_dup(str1, str1[i], j) != -1)
-            {
-                write(1, &str1[i], 1);
-                i++;
-                j = 0;
-            }
-            else
-            {
-                i++;
-            }
-        }
-    }
+	i = 0;
+	j = 0;
+	while (str1[i] != '\0')
+	{
+		if (str1[i] != str2[j] && str2[j] != '\0')
+		{
+			j++;
+		}
+		else
+		{
+			if (is_dup(str1, str1[i], i) != -1)
+			{
+				if (str2[j] != '\0')
+				{
+					write(1, &str1[i], 1);
+				}
+				j = 0;
+			}
+			i++;
+		}
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    
-    argv[1]= "padinton";
-    argv[2]= "paqefwtdjetyiytjneytjoeyjnejeyj";
-    if (argc != 3)
-        write(1, "\n", 1);
-    else
-    {
-        ft_inter(argv[1], argv[2]);
-        write(1, "\n", 1);
-    }
-    return (0);
+	if (argc != 3)
+		write(1, "\n", 1);
+	else
+	{
+		ft_inter(argv[1], argv[2]);
+		write(1, "\n", 1);
+	}
+	return (0);
 }
 
 /*
@@ -73,9 +68,12 @@ gcc -Wall -Werror -Wextra inter.c && ./a.out | cat -e
 $
 
 
-cc -Wall -Werror -Wextra inter.c && ./a.out "padinton" "paqefwtdjetyiytjneytjoeyjnejeyj" | cat -e
-cc -Wall -Werror -Wextra inter.c && ./a.out ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd | cat -e
-cc -Wall -Werror -Wextra inter.c && ./a.out "rien" "cette phrase ne cache rien" | cat -e
+cc -Wall -Werror -Wextra inter.c
+	&& ./a.out "padinton" "paqefwtdjetyiytjneytjoeyjnejeyj" | cat -e
+cc -Wall -Werror -Wextra inter.c
+	&& ./a.out ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd | cat -e
+cc -Wall -Werror -Wextra inter.c
+	&& ./a.out "rien" "cette phrase ne cache rien" | cat -e
 cc -Wall -Werror -Wextra inter.c && ./a.out | cat -e
 
 */
